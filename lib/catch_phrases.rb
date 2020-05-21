37f9ff2850ba2c10dee176c44c57b2f4cb2f3b42
Roan Bascoe <roanbascoe5@gmail.com>
2020-05-21 20:21:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,4 +1,4 @@
- $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario
   phrase = "It's-a me, Mario!"
@@ -7,7 +7,7 @@ end
 
 
 def toadstool
-  puts status
+  puts $status
 end
 
 
